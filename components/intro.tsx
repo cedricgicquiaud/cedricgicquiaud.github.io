f9e7eda72c6346a44fafa4d59748f3061d17b2fd
CedricPro <maisonepigenetic@gmail.com>
2026-08-29 17:34:30 +0200

    feat: intro links to GitHub, LinkedIn and mailto from site.json (GREEN)

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,6 +5,17 @@ export function Intro() {
     <section id="intro">
       <h1>{site.name}</h1>
       <p>{site.title}</p>
+      <ul>
+        <li>
+          <a href={site.links.github}>GitHub</a>
+        </li>
+        <li>
+          <a href={site.links.linkedin}>LinkedIn</a>
+        </li>
+        <li>
+          <a href={`mailto:${site.email}`}>Mail</a>
+        </li>
+      </ul>
     </section>
   );
 }
