8faf8e62c76394d3813f6a9b403ca942e2adf254
CedricPro <maisonepigenetic@gmail.com>
2026-08-30 08:16:19 +0200

    feat: two-column grid on home with sticky left column (GREEN)

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,18 @@ import { Projects } from "@/components/projects";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col">
-      <Intro />
-      <Portrait />
-      <About />
-      <Experience />
-      <Projects />
+    <main className="mx-auto w-full max-w-6xl flex-1 px-6 lg:px-12">
+      <div className="lg:grid lg:grid-cols-[minmax(0,2fr)_minmax(0,3fr)] lg:gap-16">
+        <div className="flex flex-col justify-between py-16 lg:sticky lg:top-0 lg:h-screen lg:py-24">
+          <Intro />
+        </div>
+        <div>
+          <Portrait />
+          <About />
+          <Experience />
+          <Projects />
+        </div>
+      </div>
     </main>
   );
 }
