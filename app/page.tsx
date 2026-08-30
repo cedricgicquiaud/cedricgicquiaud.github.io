ab54cef2d06244f6986a8dfc06e800b018c9f729
CedricPro <maisonepigenetic@gmail.com>
2026-08-30 11:34:03 +0200

    feat: move portrait into the left column at 160px (GREEN)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { About } from "@/components/about";
 import { Experience } from "@/components/experience";
 import { Intro } from "@/components/intro";
-import { Portrait } from "@/components/portrait";
 import { Projects } from "@/components/projects";
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
           <Intro />
         </div>
         <div>
-          <Portrait />
           <About />
           <Experience />
           <Projects />
